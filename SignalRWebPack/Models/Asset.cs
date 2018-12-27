cf7dfbdf637e4fa7d5d88ff711e9e3bcb2b5374a
Richard Bray <rjbray1@gmail.com>
2018-12-27 21:12:34 +0000

    added EF model

---
+++ SignalRWebPack/Models/Asset.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SignalRWebPack.Models
+{
+    public partial class Asset
+    {
+        public int AssetId { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string Exchange { get; set; }
+        public decimal? PriceLast { get; set; }
+        public decimal? PriceHigh { get; set; }
+        public decimal? PriceLow { get; set; }
+        public decimal? PriceChange { get; set; }
+        public string PriceChangePercent { get; set; }
+        public string Volume { get; set; }
+        public DateTime Updated { get; set; }
+        public string AvgVolume3m { get; set; }
+        public string MarketCap { get; set; }
+        public string Revenue { get; set; }
+        public decimal? Peratio { get; set; }
+        public decimal? Beta { get; set; }
+    }
+}
---
+++ SignalRWebPack/Models/UmbrellaContext.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SignalRWebPack.Models
+{
+    public partial class UmbrellaContext : DbContext
+    {
+        public UmbrellaContext()
+        {
+        }
+
+        public UmbrellaContext(DbContextOptions<UmbrellaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Asset> Asset { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=NUC-RICHARD;Database=Umbrella;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Asset>(entity =>
+            {
+                entity.HasKey(e => e.AssetId)
+                    .HasName("PK__Asset__43492353EB26DC6B")
+                    .ForSqlServerIsClustered(false);
+
+                entity.HasAnnotation("SqlServer:MemoryOptimized", true);
+
+                entity.Property(e => e.AvgVolume3m)
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Beta).HasColumnType("smallmoney");
+
+                entity.Property(e => e.Exchange)
+                    .IsRequired()
+                    .HasMaxLength(14)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.MarketCap)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Peratio)
+                    .HasColumnName("PEratio")
+                    .HasColumnType("smallmoney");
+
+                entity.Property(e => e.PriceChange).HasColumnType("smallmoney");
+
+                entity.Property(e => e.PriceChangePercent)
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PriceHigh).HasColumnType("smallmoney");
+
+                entity.Property(e => e.PriceLast).HasColumnType("smallmoney");
+
+                entity.Property(e => e.PriceLow).HasColumnType("smallmoney");
+
+                entity.Property(e => e.Revenue)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Symbol)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Updated).HasColumnType("datetime");
+
+                entity.Property(e => e.Volume)
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
